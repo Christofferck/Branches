0c9af42a4835382a0af9f79ace03aa332cf14907
Christofferck <70515469+Christofferck@users.noreply.github.com>
2020-09-08 11:52:19 +0200

    Add files via upload

---
+++ GitMap/isprime.js
@@ -0,0 +1,13 @@
+"use strict";
+let x = Number(prompt("Enter value", "0"));
+
+let start = new Date();
+let b = isPrime(x);
+let stop = new Date();
+let elapsed = stop - start;
+
+document.write("Is it a prime number? ")
+document.write(b, elapsed);
+
+
+
